ba2b60107094a9cfae1082d331da46a519c3af7c
Ewout <we.baars@student.avans.nl>
2021-09-14 12:20:59 +0200

    simple client and server added

---
+++ SimpleGame/Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGame
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 1330);
+            bool done = false;
+            Console.WriteLine("Type 'bye' to end connection");
+            while (!done)
+            {
+                Console.Write("Enter a message to send to server: ");
+                string message = Console.ReadLine();
+
+                WriteTextMessage(client, message);
+
+                string response = ReadTextMessage(client);
+                Console.WriteLine("Response: " + response);
+                done = response.Equals("BYE");
+            }
+        }
+
+        public static void WriteTextMessage(TcpClient client, string message)
+        {
+            var stream = new StreamWriter(client.GetStream(), Encoding.ASCII);
+            {
+                stream.WriteLine(message);
+                stream.Flush();
+            }
+        }
+
+        public static string ReadTextMessage(TcpClient client)
+        {
+            var stream = new StreamReader(client.GetStream(), Encoding.ASCII);
+            {
+                return stream.ReadLine();
+            }
+        }
+    }
+}
